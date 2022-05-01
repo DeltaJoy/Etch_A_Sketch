cd3506409c479d5c32dc9b89a6edd5e874dd3614
DeltaJoy <j.komorowski1991@gmail.com>
2022-05-01 14:33:44 -0500

    updated functions
    i separated the loop function to draw the grid from the ask prompt question for grid size.
    
    I will keep the code crossed out for now but I needed to use a simple example to figure out how saving a function as a variable and using that variable as a parameter in the whole function.
    
    I created a whole function to run everything:
    1. to ask how big of grid
    2. return that value and save it as a parameter
    3. use the parameter in grid creation function.

--- js.js
+++ js.js
@@ -3,42 +3,68 @@
 document.getElementById("ten").addEventListener("click", start);
 document.getElementById("clear").addEventListener("click", clearBoard);
 
+// function adding(x,y){
+//     let total = (x) + y 
+//     return total
+// }
+
+// function finalAdd (returnValue) {
+//     console.log(returnValue + 2)
+// }
+
+// function make(x,y) {
+//     let results = adding(x,y)
+//     finalAdd(results)
+
+// }
+
 
 
 function start () {
-    createGrid()
+    let num = createGrid();
+    grid (num);
     changeColor();
-
 }
 
 
 function createGrid() {
     let totalSize = prompt("gridsize?")
-    if (totalSize > 60) {
-        prompt("sorry thats too many to load, choose a smaller number")
-    } else if (totalSize < 61) {
+    if (totalSize < 61) {
+        return totalSize
+        // let parseToInto = parseInt(totalSize)
+        // return parseToInto;
 
-    
-    
-        for(let i = 1; i < totalSize*totalSize +1; i++) {
-            let newBox = document.createElement("div");
-            newBox.classList.add("singleBox");
-            document.getElementById("container").style.gridTemplateColumns = `repeat(${totalSize}, auto)`
-            document.getElementById("container").style.gridTemplateRows = `repeat(${totalSize}, auto)`
-            document.getElementById("container").append(newBox)
 
-            // document.querySelectorAll(".singleBox").forEach(oneBox => {
-            //     oneBox.addEventListener("mouseover", () => {
-            //         oneBox.style.backgroundColor = "black"
-            //     })
-            // })
-            
+        // for(let i = 1; i < totalSize*totalSize +1; i++) {
+        //     let newBox = document.createElement("div");
+        //     newBox.classList.add("singleBox");
+        //     document.getElementById("container").style.gridTemplateColumns = `repeat(${totalSize}, auto)`
+        //     document.getElementById("container").style.gridTemplateRows = `repeat(${totalSize}, auto)`
+        //     document.getElementById("container").append(newBox)
+        // }
+    }   
+    else {
+        let askAgain = prompt("sorry thats too many to load, choose a smaller number")
+        if (askAgain > 60) {
+            alert("maybe play later")
+        } else {
+            return askAgain
         }
-    }
+        
+       
+        
+    } 
     
+}
 
-    
-    
+function grid (num) {
+    for(let i = 1; i < num*num +1; i++) {
+        let newBox = document.createElement("div");
+        newBox.classList.add("singleBox");
+        document.getElementById("container").style.gridTemplateColumns = `repeat(${num}, auto)`
+        document.getElementById("container").style.gridTemplateRows = `repeat(${num}, auto)`
+        document.getElementById("container").append(newBox)
+    }
 }
 
 
